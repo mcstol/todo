3de9016ab90505a5aa5d4bd56ff4a7c8fe774066
Marcelo Oliveira <santos.marcelooliveira@gmail.com>
2021-04-18 23:24:22 -0300

    feat: remove task

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const ulElement = document.querySelector("#tasks ul")
 const tasks = []
 
+const removeTask = (index) => {
+    tasks.splice(index, 1)
+    const taskElement = document.querySelector(`ul li#item-${index}`)
+    ulElement.removeChild(taskElement)
+}
+
 const addTask = (text) => {
-   tasks.push(text)
+   const total = tasks.push(text)
+   const index = total - 1
 
    const taskElement = document.createElement("li")
    const textElement = document.createElement("span")
    const btnElement = document.createElement("button")
+
+   taskElement.setAttribute("id", `item-${index}`)
+
+   btnElement.onclick = () =>{
+       removeTask(index)
+       console.log(tasks)
+   }
 
    taskElement.appendChild(textElement)
    taskElement.appendChild(btnElement)
@@ -17,10 +31,6 @@ const addTask = (text) => {
    ulElement.appendChild(taskElement)
 
 
-}
-
-const removeTask = (index) => {
-    tasks.splice(index, 1)
 }
 
 const btnAdd = document.querySelector("#form button")
